9cee449afea669cacbd13e8e6baa509c6663163e
Saurabh-827 <saurabhrabta@gmail.com>
2025-06-20 21:52:35 +0530

    Added some more comments for proper understnding

--- 1.FunctionalComponents/FunctionalComponent.tsx
+++ 1.FunctionalComponents/FunctionalComponent.tsx
@@ -1,7 +1,7 @@
-//a simple functional component that returns a h1 element. 
+//a simple functional component that returns a h1 element.
 // it is a function that returns a jsx element(Basic Example (Without TypeScript))
 function HelloWorld() {
-    return <h1>Hello, World!</h1>
+	return <h1>Hello, World!</h1>;
 }
 //Difference between regular function and functional component
 
@@ -11,33 +11,33 @@ function HelloWorld() {
 // | Purpose                         | General-purpose logic (calculations, utilities) | Defines part of a UI     |
 // | Can use hooks (e.g. `useState`) | ❌ No                                          | ✅ Yes                  |
 
-
 /*TYPESCRIPT FUNCTIONAL COMPONENTS*/
 
+//1. Typing Props with an interface
+// Interface : Interface is used to create a "shape or structure of object"
+//  (i call it frame😁 for my understanding) because whenever you have to create
+// an object just use that variable to this frame and now you are free to use all
+// keys  from that interface for assigning values
 
-
-//1. Typing Props with an interface 
-                    // Interface : Interface is used to create a "shape or structure of object" 
-                    //  (i call it frame😁 for my understanding)
-
-
-// As you overall structure of this interface ,  same as object right?
-interface HelloProps { //here we declared a interface for props 
-    name: string;        // here you can see that key(variables) and values(types)
-    age?: number; //optional prop
-    children?: React.ReactNode; 
-}  // why we use this interface ?
+// As you see overall structure of this interface ,  same as object right?
+interface HelloProps {
+	//here we declared a interface for props
+	name: string; // here you can see that key(variables) and values(types)
+	age?: number; //optional prop
+	children?: React.ReactNode;
+} // why we use this interface ?
 // ANS: We using react + Typescript so we need to describe the types of props (need of type in TypeScript)😒 otherwise errorrrr
 
-function Hello({name, age}: HelloProps) {  // as you know simple function looks like this Hello() {}
-    //but you can see that in the parameter we passed {name, age}: HelloProps this means that types of name and age 
-    //are described in the interface HelloProps so compiler don't trouble for type of name and age 
-    return (
-        <div>
-            <p>Hello, {name}!</p>
-            {age && <p>Age: {age}</p>}
-        </div>
-    );
+function Hello({ name, age }: HelloProps) {
+	// as you know simple function looks like this Hello() {}
+	//but you can see that in the parameter we passed {name, age}: HelloProps this means that types of name and age
+	//are described in the interface HelloProps so compiler don't trouble for type of name and age
+	return (
+		<div>
+			<p>Hello, {name}!</p>
+			{age && <p>Age: {age}</p>}
+		</div>
+	);
 }
 
 //2. Using React.FC (FunctionComponent)
@@ -49,13 +49,15 @@ function Hello({name, age}: HelloProps) {  // as you know simple function looks 
 //     </div>
 // )
 //2.1 Using React.FC (FunctionComponent) trying to do without arrow Fucnction
-//converted arrow function to normal function   
-const Hello2: React.FC<HelloProps> = function ({ name, age , children}) { //by using function keyword and then removed arrow
-    // and uses return keyword
-    return (<div>                        
-        <p>Hello, {name}!</p>
-        {age && <p>Age: {age}</p>}
-        {children}
-    </div>
-);
-}
+//converted arrow function to normal function
+const Hello2: React.FC<HelloProps> = function ({ name, age, children }) {
+	//by using function keyword and then removed arrow
+	// and uses return keyword
+	return (
+		<div>
+			<p>Hello, {name}!</p>
+			{age && <p>Age: {age}</p>}
+			{children}
+		</div>
+	);
+};
